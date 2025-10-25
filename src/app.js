89ced24ef295f54b3ce73582c217828b1d7df226
Eduard-jcs <eduardjcs14@gmail.com>
2025-10-25 01:52:43 +0000

    Hallowen Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+
+  let who = ['Una Bruja', 'Un Polaco', 'El Guardia', 'La Mamá de Tarzán'];
+  let action = ['rompió', 'quebró', 'lanzó', 'quemó'];
+  let what = ['mi tarea', 'mi teléfono', 'el carrito'];
+  let when = ['antes de la clase', 'cuando estaba con el cuero', 'mientras estaba en el monte', 'mientras peleaba', 'cuando jugaba pelota'];
+
+  let generatorElement = document.querySelector("#generator");
+  generatorElement.innerHTML = `${seleccionarAleatorio(who)} ${seleccionarAleatorio(action)} ${seleccionarAleatorio(what)} ${seleccionarAleatorio(when)}`;
 };
+
+function seleccionarAleatorio(lista) {
+  let elementoAleatorio = Math.floor(Math.random() * lista.length);
+  return lista[elementoAleatorio];
+}
+
+ 
